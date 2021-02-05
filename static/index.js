5986c1a743712272898bdf71d1ce8e21bc9da423
thewarsawpakt <smallperson07@gmail.com>
2021-02-05 12:50:50 -0600

    dude it works

--- static/index.js
+++ static/index.js
@@ -1,13 +1,13 @@
 var socket = io();
-socket.on('connect', function() {
-    socket.emit('my event', {data: 'I\'m connected!'});
-});
 
 function sendmessage(){
     date = new Date()
+    if(document.getElementById("messagebox").value == null){
+        return
+    }
     var messagebody = {
         "body": document.getElementById("messagebox").value,
-        "user" :  document.getElementById("usernamebox").value,
+        "user" :  "pakt",
         "date": date
     }
     socket.emit("message_send", messagebody)
@@ -16,7 +16,10 @@ socket.on("message_recv", data => {
     var message = document.createElement("div");
     message.innerHTML = `<div class="message">${data["user"]}: ${data["body"]}<span></span></div>`
     document.getElementById("messages").appendChild(message)
-    var element = document.getElementById("messages");
-    var messageBody = document.querySelector('#messages');
     $(document).scrollTop($(document).height()); 
+    $("#messagecontainer2")[0].reset();
+});
+
+$("#messagecontainer2").submit(function(e) {
+    e.preventDefault();
 });
