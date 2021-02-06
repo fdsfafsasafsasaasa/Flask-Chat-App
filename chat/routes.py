6eb47c2fb76cb4764046a627f70d0636af84c91e
thewarsawpakt <smallperson07@gmail.com>
2021-02-06 01:09:11 -0600

    Added database for persistence.

--- chat/routes.py
+++ chat/routes.py
@@ -2,14 +2,18 @@ from flask_socketio import SocketIO, send, emit
 from flask import Flask, render_template
 from chat import socketapp, app
 from datetime import datetime
+import sqlite3
 
-messages = []
+conn = sqlite3.connect("messages.db")
+c = conn.cursor()
 
 @app.route("/")
-def root(data=None):
-    return render_template("index.html", messages=messages)
+def root(data=None):    
+    return render_template("index.html", messages=c.execute("SELECT * FROM messages").fetchall())
 
 @socketapp.on('message_send')
 def message_send(message):
-    messages.append(message)
+    print(c.execute("SELECT * FROM messages").fetchall())
+    c.execute("INSERT INTO messages VALUES (?, ?)", (message['user'], message['body']))
+    conn.commit()
     emit("message_recv", message, broadcast=True)
